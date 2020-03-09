6df4774fe5ae6047d7d997b5fcf48732a15e82df
Kevin Allen <kevin.allen91@gmail.com>
2020-03-09 13:44:41 -0700

    day 1 mvp complete

--- src/hashtable.py
+++ src/hashtable.py
@@ -1,21 +1,24 @@
 # '''
 # Linked List hash table key/value pair
 # '''
+
+
 class LinkedPair:
     def __init__(self, key, value):
         self.key = key
         self.value = value
         self.next = None
 
+
 class HashTable:
     '''
     A hash table that with `capacity` buckets
     that accepts string keys
     '''
+
     def __init__(self, capacity):
         self.capacity = capacity  # Number of buckets in the hash table
         self.storage = [None] * capacity
-
 
     def _hash(self, key):
         '''
@@ -25,7 +28,6 @@ class HashTable:
         '''
         return hash(key)
 
-
     def _hash_djb2(self, key):
         '''
         Hash an arbitrary key using DJB2 hash
@@ -34,14 +36,13 @@ class HashTable:
         '''
         pass
 
-
     def _hash_mod(self, key):
+        # takes hash turns into index
         '''
         Take an arbitrary key and return a valid integer index
         within the storage capacity of the hash table.
         '''
         return self._hash(key) % self.capacity
-
 
     def insert(self, key, value):
         '''
@@ -51,9 +52,15 @@ class HashTable:
 
         Fill this in.
         '''
-        pass
-
-
+        # hash key to make index
+        index = self._hash(key)
+        # if hashed key already exists == collision
+        if self.storage[index] is not None:
+            print('Error: item at index')
+        else:
+            # else add key value to hashed key location
+            self.storage[index] = (key, value)
+            return
 
     def remove(self, key):
         '''
@@ -63,8 +70,25 @@ class HashTable:
 
         Fill this in.
         '''
-        pass
-
+        # hash key
+        index = self._hash(key)
+        old_item = None
+        # if hash exists
+        if self.storage[index] is not None:
+            # and if is correct key
+            if self.storage[index][0] == key:
+                # set old item to be deleted value
+                old_item = self.storage[index]
+                # set to None
+                self.storage[index] = None
+            # if not correct key, collision
+            else:
+                print('Error: collision occurred')
+        # else key doesn't exist
+        else:
+            print("Error: key not found ")
+        # return deleted value
+        return old_item
 
     def retrieve(self, key):
         '''
@@ -74,8 +98,21 @@ class HashTable:
 
         Fill this in.
         '''
-        pass
+        # hash key
+        index = self._hash(key)
+        # if hash exists
+        if self.storage[index] is not None:
+            # if key is correct hash
+            if self.storage[index][0] == key:
+                # return value at key
+                return self.storage[index][1]
+            else:
+                print('Error: collision')
+        # if hash doesn't exist, return none
+        else:
+            return None
 
+        return
 
     def resize(self):
         '''
@@ -84,8 +121,16 @@ class HashTable:
 
         Fill this in.
         '''
-        pass
+        # set old storage in variable to iterate through
+        old_storage = self.storage
+        # update storage
+        self.capacity *= 2
+        self.storage = [None] * self.capacity
 
+        # iterate through old storage, inserting key, values into new storage
+        for item in old_storage:
+            if item is not None:
+                self.insert(item[0], item[1])
 
 
 if __name__ == "__main__":
